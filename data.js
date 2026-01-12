d2b52c5187847bf53591fbdab48be444152fe423
Muniru Adam <madam31@academic.rrc.ca>
2026-01-11 20:27:06 -0600

    feat: create data.js with Employee data

---
+++ data.js
@@ -0,0 +1,25 @@
+// Employee and Department data
+const departments = [
+    {
+        name: "Engineering",
+        employees: [
+            { firstName: "Priyanka", lastName: "Bose" },
+            { firstName: "Madeleine", lastName: "Madden" },
+            { firstName: "Taylor", lastName: "Napier" }
+        ]
+    },
+    {
+        name: "IT Technicians",
+        employees: [
+            { firstName: "Iniko", lastName: "Stonebear" },
+            { firstName: "Tala", lastName: "Braveheart" }
+        ]
+    },
+    {
+        name: "Human Resources",
+        employees: [
+            { firstName: "Jesse", lastName: "Azure" },
+            { firstName: "Stacy", lastName: "Silver" }
+        ]
+    }
+];
