e7ad53d301ddb13260da938981408dff1c1955ae
Muniru Adam <madam31@academic.rrc.ca>
2026-01-26 17:18:07 -0600

    feat: update app.tsx file

--- react-employee-directory/src/App.tsx
+++ react-employee-directory/src/App.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import EmployeeList from './components/EmployeeList';
+import { departments as initialDepartments } from './data/employees';
+import type { Department } from './types/Employee';
 import './App.css';
 
 function App() {
+  const [departments, setDepartments] = useState<Department[]>(initialDepartments);
+
   return (
     <div className="app">
       <Header />
-      <EmployeeList />
+      <EmployeeList departments={departments} />
       <Footer />
     </div>
   );
