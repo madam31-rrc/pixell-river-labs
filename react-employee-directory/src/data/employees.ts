9d563d80da570ee0ddbb65cb031584e23220de82
Muniru Adam <madam31@academic.rrc.ca>
2026-01-26 22:07:23 -0600

    feat: update employee files

--- react-employee-directory/src/data/employees.ts
+++ react-employee-directory/src/data/employees.ts
@@ -6,21 +6,28 @@ export const departments: Department[] = [
     employees: [
       { firstName: "Priyanka", lastName: "Bose" },
       { firstName: "Madeleine", lastName: "Madden" },
-      { firstName: "Taylor", lastName: "Napier" }
+      { firstName: "Taylor", lastName: "Napier" },
+      { firstName: "Alex", lastName: "Johnson" },
+      { firstName: "Sam", lastName: "Williams" },
+      { firstName: "Jordan", lastName: "Smith" } 
     ]
   },
   {
     name: "IT Technicians",
     employees: [
       { firstName: "Iniko", lastName: "Stonebear" },
-      { firstName: "Tala", lastName: "Braveheart" }
+      { firstName: "Tala", lastName: "Braveheart" },
+      { firstName: "Kiona", lastName: "Redhawk" },
+      { firstName: "Liam", lastName: "Brown" }
     ]
   },
   {
     name: "Human Resources",
     employees: [
       { firstName: "Jesse", lastName: "Azure" },
-      { firstName: "Stacy", lastName: "Silver" }
+      { firstName: "Stacy", lastName: "Silver" },
+      { firstName: "Morgan", lastName: "Gold" },
+      { firstName: "Casey", lastName: "Copper" }
     ]
   }
 ];
