1d36275df4c0b37335176f2d9e0ce8c4657f9881
Muniru Adam <madam31@academic.rrc.ca>
2026-03-30 20:36:29 -0500

    feat: Initialize organization repository and handle role additions

--- react-employee-directory/src/App.tsx
+++ react-employee-directory/src/App.tsx
@@ -5,20 +5,37 @@ import EmployeeList from './components/EmployeeList';
 import AddEmployeeForm from './components/AddEmployeeForm';
 import Organization from './components/Organization';
 import { employeeRepo } from './repositories/employeeRepo';
+import { organizationRepo } from './repositories/organizationRepo';
 import { departments as initialDepartments } from './data/employees';
+import { organizationData as initialOrganizationData } from './data/organization';
 import './App.css';
 
 function App() {
-  const [refreshKey, setRefreshKey] = useState(0);
+  const [employeeRefreshKey, setEmployeeRefreshKey] = useState(0);
+  const [organizationRefreshKey, setOrganizationRefreshKey] = useState(0);
 
+  // Initialize repositories with data on first load
   useEffect(() => {
-    employeeRepo.setDepartments(initialDepartments);
+    const departmentsCopy = JSON.parse(JSON.stringify(initialDepartments));
+    employeeRepo.setDepartments(departmentsCopy);
+    
+    const organizationCopy = JSON.parse(JSON.stringify(initialOrganizationData));
+    organizationRepo.setRoles(organizationCopy);
+    
+    setEmployeeRefreshKey(prev => prev + 1);
+    setOrganizationRefreshKey(prev => prev + 1);
   }, []);
 
+  // Get data from repositories
   const departments = employeeRepo.getDepartments();
 
+  // Callbacks to refresh views
   const handleEmployeeAdded = () => {
-    setRefreshKey(prev => prev + 1);
+    setEmployeeRefreshKey(prev => prev + 1);
+  };
+
+  const handleRoleAdded = () => {
+    setOrganizationRefreshKey(prev => prev + 1);
   };
 
   return (
@@ -29,7 +46,7 @@ function App() {
           <Route 
             path="employees" 
             element={
-              <div key={refreshKey}>
+              <div key={employeeRefreshKey}>
                 <EmployeeList departments={departments} />
                 <AddEmployeeForm 
                   departments={departments} 
@@ -38,7 +55,15 @@ function App() {
               </div>
             } 
           />
-          <Route path="organization" element={<Organization />} />
+          <Route 
+            path="organization" 
+            element={
+              <Organization 
+                refreshKey={organizationRefreshKey}
+                onRoleAdded={handleRoleAdded}
+              />
+            } 
+          />
         </Route>
       </Routes>
     </BrowserRouter>
